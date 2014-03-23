1cbf21a79550ed45c67cc7a819b20f188ebdf08e
0x0agon <0x0agon@gmail.com>
2014-03-23 06:25:51 +0000

    Added callbacks to my events, which adds threads to the program which will work in the background. Basically added interrupt functionality to the encoderTest file. Will be adding it to the RobotTest file

--- encoderTest.py
+++ encoderTest.py
@@ -3,10 +3,10 @@ import carttracker
 import motordriver
 
 gpio.setup("P8_17", gpio.IN) #This sets up the pin we want to use to listen to the encoder
-gpio.add_event_detect("P8_17", gpio.BOTH)
+#gpio.add_event_detect("P8_17", gpio.BOTH, callback=event1Callback)
 
 gpio.setup("P8_13", gpio.IN)
-gpio.add_event_detect("P8_13", gpio.BOTH)
+#gpio.add_event_detect("P8_13", gpio.BOTH, callback=event2Callback)
 
 driver = motordriver.MotorDriver()
 driver.initialize()
@@ -14,17 +14,27 @@ driver.initialize()
 cart = carttracker.CartTracker()
 cart.initialize(driver)
 
+def event1Callback(channel):
+    cart.updateState(gpio.input("P8_17"), gpio.input("P8_13"))
+    cart.getEncoderUpdate()
+
+def event2Callback(channel):
+    cart.getEncoderUpdate()
+
+gpio.add_event_detect("P8_17", gpio.BOTH, callback=event1Callback)
+gpio.add_event_detect("P8_13", gpio.BOTH, callback=event2Callback)
 
 while True:
-    if gpio.event_detected("P8_17"):
-        cart.updateState(gpio.input("P8_17"),gpio.input("P8_13"))
-        cart.getEncoderUpdate()
-        #print cart.encoderTicCounter, cart.currentPosition, cart.currentVelocity, cart.elapsedTime
-        print 'direction= ', cart.direction, 'encoder= ', cart.encoderTicCounter
-    else:
-        pass
-    if gpio.event_detected("P8_13"):
-        cart.updateState(gpio.input("P8_17"),gpio.input("P8_13"))
+    #if gpio.event_detected("P8_17"):
+    #    cart.updateState(gpio.input("P8_17"),gpio.input("P8_13"))
+    #    cart.getEncoderUpdate()
+    #    #print cart.encoderTicCounter, cart.currentPosition, cart.currentVelocity, cart.elapsedTime
+    #    print 'direction= ', cart.direction, 'encoder= ', cart.encoderTicCounter
+    #else:
+    #    pass
+    #if gpio.event_detected("P8_13"):
+    #    cart.updateState(gpio.input("P8_17"),gpio.input("P8_13"))
         
-    else:
-        pass        
+    #else:
+    #    pass        
+    print cart.direction
